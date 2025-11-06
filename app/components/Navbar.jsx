82c12f57ea473290f669f7b107119036f7bdec73
jonayed-nasir <junayednasir073@gmail.com>
2025-11-06 23:41:21 +0600

    3.30

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -13,10 +13,11 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      <div className=" absolute z-20 w-full left-0 top-0">
+      <div className="  z-20 w-full left-0 top-0 fixed">
         {/* -----------Nav Bar---------- */}
         <div className="w-full h-[70px]  flex items-center">
-          <div className="w-full bg-white/5 backdrop-blur-sm  max-w-[99%] md:max-w-[80%] mx-auto flex items-center justify-between py-4 px-5 rounded-2xl">
+
+          <div className="w-full bg-transparent  backdrop-blur-sm  max-w-[99%] md:max-w-[80%] mx-auto flex items-center justify-between py-4 px-5 rounded-2xl">
             {/* -------Logo------- */}
             <div className="min-[50px] text-white text-2xl font-bold flex items-center">
               <Image src="/logo.png" alt="logo" width={70} height={70} />{' '}
@@ -31,77 +32,73 @@ export default function Navbar() {
 
             {/* -------Nav Link---------- */}
             <div
-              className={`absolute top-[100px] left-5 w-[40%] rounded-2xl bg-white/30 lg:bg-white/0 backdrop-blur-3xl lg:backdrop-blur-none  flex flex-col lg:items-center gap-3 lg:gap-6 py-6 px-6 lg:px-0 text-black lg:text-white   transition-all delay-300 duration-300 lg:static lg:flex-row   lg:py-0 lg:flex  ${
+              className={`absolute  top-[100px] left-5 w-[40%] bg-black/30 lg:bg-black/0 rounded-2xl  flex flex-col lg:items-center gap-4 lg:gap-6 py-6 px-6 lg:px-0 text-black lg:text-white font-bold transition-all delay-300 duration-300 lg:static lg:flex-row lg:py-0 lg:flex  ${
                 menuOpen ? 'flex justify-end' : 'hidden'
               } justify-end`}
             >
-              <span className="navHovereffect relative">
-                <a className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+              <span className="navHovereffect relative inline-block">
+                <a
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('home')}
                 </a>
               </span>
-              <span className="navHovereffect relative">
-                <a 
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+              <span className="navHovereffect relative inline-block">
+                <a
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
-              
                 >
-                {t('about')} 
+                  {t('about')}
                 </a>
               </span>
-              <span className="navHovereffect relative">
+              <span className="navHovereffect relative inline-block">
                 <a
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('departments')}
                 </a>
               </span>
-              <span className="navHovereffect relative">
+              <span className="navHovereffect relative inline-block">
                 <a
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('academic')}
                 </a>
               </span>
-              <span className="navHovereffect relative">
+              <span className="navHovereffect relative inline-block">
                 <a
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('admission')}
-                  
                 </a>
               </span>
-              <span className="navHovereffect relative">
+              <span className="navHovereffect relative inline-block">
                 <a
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('notice')}
-                  
                 </a>
               </span>
-              <span className="navHovereffect relative">
+              <span className="navHovereffect relative inline-block">
                 <a
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('donate')}
-                  
                 </a>
               </span>
 
-              <span className="navHovereffect relative">
+              <span className="navHovereffect relative inline-block">
                 <a
-                  className="hover:text-gray-300 transition-all duration-300 hover:bg-white/5 backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
+                  className="hover:text-gray-600 lg:text-gray-300 bg-white/40 lg:bg-white/0  transition-all duration-300  backdrop-blur-3xl py-1.5 px-1.5 rounded-sm"
                   href="#"
                 >
                   {t('contact')}
-                  
                 </a>
               </span>
               <LanguageSwitcher className="bg-transparent backdrop-blur-2xl" />
--- app/pages/Introduction.jsx
+++ app/pages/Introduction.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Introduction() {
   return (
-    <div>
+    <div className='mt-2 lg:mt-20 py-10 md:py-16 px-3 lg:py-20 w-full max-w-[95%] lg:max-w-[80%] mx-auto flex flex-col justify-center items-center'>
+      <div className='space-y-7 md:space-y-12 lg:space-y-20 bg-[#EFE9E3] py-5 md:py-10 px-3 md:px-5 rounded-md rounded-tr-4xl rounded-bl-4xl border-b-8 border-[#D9CFC7] '>
 
+        <h3 className='text-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-white rounded-tl-4xl rounded-br-4xl rounded-sm bg-pink-500 py-3 border-b-8 border-pink-600 '>সংক্ষিপ্ত পরিচিতি</h3>
+
+        <p className='text-gray-600 text-lg md:text-2xl  line-clamp-6 text-center'>একমাত্র ইসলাম। জাগতিক চাহিদা, লোভ-লালসা এবং নশ্বর জগতের ক্ষণস্থায়ী সাজসজ্জা ও সৌন্দর্যের অবান্তর মোহে এক শ্রেণীর মানুষ সর্বযুগেই তাদের কূটকৌশল ও ষড়যন্ত্রের জাল বিস্তার করে কল্যাণ ও মুক্তির আদর্শবাহী ইসলামকে ধরাপৃষ্ঠ থেকে বিলুপ্ত করতে চেষ্টার কোনো ত্রুটি করেনি। এর পাশাপাশি নির্ভেজাল, চিরন্তন ও মহাসত্য ইসলামের নিশানবরদার আল্লাহর মদদপুষ্ট অন্য একটি দল পার্থিব সকল স্বার্থ জলাঞ্জলি দিয়ে জীবনের সর্বস্ব অকাতরে বিলীন করে আল্লাহর জমিনে আল্লাহ তাআলার দ্বীন কায়েম করতে উৎসর্গিত ছিলেন। তাঁদেরই অদম্য স্পৃহা ও রক্ত নিংড়ানো শ্রমের অবদানে পৃথিবীর বুকে গড়ে উঠেছে ইসলাম ও ইসলামী ঐতিহ্যের নিরঘুশ ধারাবাহিকতা সংরক্ষণের অগণিত দুর্জয় দূর্গ খাটি দ্বীনি প্রতিষ্ঠান। এই সূত্র পরস্পরায় ১৯৯০ ইং সালে তাবলীগ জামাতের বিশিষ্ট মুরব্বী নারায়ণগঞ্জ আমলাপাড়া মাদরাসার স্বনামধন্য মুহতামিম হযরত মাওলানা হুসাইন আহমাদ (রহ.) এর দিক-নির্দেশনায় মো: বাদশা মিয়া সাহেবের দানকৃত বিস্তৃত ভূমির উপরে বিশাল অবয়বে প্রতিষ্ঠা লাভ করে “জামিয়াতুস সুন্নাহ “|</p>
+        <span className='mt-8 block max-w-[150px] text-center mx-auto  px-3 rounded-tl-4xl border-b-5 border-blue-800 hover:border-blue-400 rounded-br-3xl rounded-sm  py-2 bg-blue-500 hover:bg-blue-700 transition-all duration-300'><a className='text-white  font-bold' href="#">Show More</a></span>
+      </div>
     </div>
-  )
+  );
 }
