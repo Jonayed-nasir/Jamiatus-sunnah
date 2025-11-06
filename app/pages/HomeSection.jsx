60ce2feb258722848d6e2cb500e32171372a0b0d
jonayed-nasir <junayednasir073@gmail.com>
2025-11-06 22:13:15 +0600

    3.20

--- app/pages/HomeSection.jsx
+++ app/pages/HomeSection.jsx
@@ -19,32 +19,38 @@ export default function HomeSection() {
         />
       </div>
 
-      <div className="relative w-full h-screen flex space-y-5 flex-col px-3 justify-center items-center">
-        <h1 className="text-white font-bold text-4xl lg:text-6xl text-center">
-          <Typewriter
-            options={{
-              strings: ['jamiatus sunnah', 'جامعة السنة', 'জামিয়াতুস সুন্নাহ'],
-              autoStart: true,
-              loop: true,
-             delay: 280,
-             deleteSpeed: 300
-            }}
-          />
-        </h1>
+      <div className="relative w-full h-screen flex flex-col px-3 justify-center items-center">
+        <div className='space-y-5 bg-transparent backdrop-blur-sm py-2 px-3 rounded-2xl'>
+          <h1 className="text-white font-bold text-4xl lg:text-6xl text-center">
+            <Typewriter
+              options={{
+                strings: [
+                  'jamiatus sunnah',
+                  'جامعة السنة',
+                  'জামিয়াতুস সুন্নাহ',
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 280,
+                deleteSpeed: 300,
+              }}
+            />
+          </h1>
 
-        <h3 className='text-gray-300 text-lg text-center'>
-          <Typewriter
-            options={{
-              strings: [
-                'dakhin kandi, bahadurpur, shibsor, madaripur',
-                'دكن كاندي، بهادور بور، شيبصر، مداري بور',
-                'দক্ষিন কান্দি, বাহাদুরপুর, শিবচর, মাদারীপুর',
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </h3>
+          <h3 className="text-gray-300 text-lg text-center">
+            <Typewriter
+              options={{
+                strings: [
+                  'dakhin kandi, bahadurpur, shibsor, madaripur',
+                  'دكن كاندي، بهادور بور، شيبصر، مداري بور',
+                  'দক্ষিন কান্দি, বাহাদুরপুর, শিবচর, মাদারীপুর',
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h3>
+        </div>
       </div>
     </div>
   );
--- app/pages/Introduction.jsx
+++ app/pages/Introduction.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Introduction() {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere fuga quo consequuntur alias velit vitae, nihil laborum rerum id temporibus illo cupiditate, voluptate expedita saepe dolores quia. Impedit, veritatis inventore?
+
     </div>
   )
 }
